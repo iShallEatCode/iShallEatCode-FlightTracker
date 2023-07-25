e12d45dee4b50bb2616c089043e12175ae0f5911
Artem Synytsia <synytsia.artem@yahoo.com>
2023-07-25 02:37:36 +0000

    WkFive_MiniProject_FlightTracker

--- src/main/java/Application/Service/FlightService.java
+++ src/main/java/Application/Service/FlightService.java
@@ -53,7 +53,8 @@ public class FlightService {
      *         inform our provide the front-end client with information about the added Flight.
      */
     public Flight addFlight(Flight flight){
-        return null;
+        Flight addedFlight = flightDAO.insertFlight(flight);
+        return addedFlight;
     }
 
     /**
@@ -70,7 +71,18 @@ public class FlightService {
      *         user should have some insight if they attempted to edit a nonexistent flight.)
      */
     public Flight updateFlight(int flight_id, Flight flight){
-        return null;
+        // Check if the flight with the given flight_id exists
+        Flight existingFlight = flightDAO.getFlightById(flight_id);
+        if (existingFlight == null) {
+            return null; // return null if flight doesn't exist
+        }
+
+        // Update the existing flight with the new data
+        flightDAO.updateFlight(flight_id, flight);
+
+        // Retrieve the updated flight from the database and return it
+        Flight updatedFlight = flightDAO.getFlightById(flight_id);
+        return updatedFlight;
     }
 
     /**
@@ -80,7 +92,8 @@ public class FlightService {
      * @return all flights in the database.
      */
     public List<Flight> getAllFlights() {
-        return null;
+        List<Flight> flights = flightDAO.getAllFlights();
+        return flights;
     }
 
     /**
@@ -92,6 +105,7 @@ public class FlightService {
      * @return all flights departing from departure_city and arriving at arrival_city.
      */
     public List<Flight> getAllFlightsFromCityToCity(String departure_city, String arrival_city) {
-        return null;
+        List<Flight> flights = flightDAO.getAllFlightsFromCityToCity(departure_city, arrival_city);
+        return flights;
     }
 }
